8393861acea3449ec67536b070d307289e7124f0
ELBS <86670001+nigevolution@users.noreply.github.com>
2026-01-21 11:57:34 -0400

    Update store.js

--- public/js/store.js
+++ public/js/store.js
@@ -22,14 +22,23 @@ const produtos = [
     desc:"Assinatura japonesa moderna. Grave firme, ataque rápido e equilíbrio perfeito para palco e estúdio.", audio:"/audio/trb-jp2.mp3"
   },
 
-  { nome:"Mayones Jabba 5 IR", preco:89, link:"https://pay.cakto.com.br/388nbge_735148", desc:"Flagship europeu com profundidade e definição profissional.", release:"2026-01-23T19:00:00"audio:"/audio/mayones-jabba-5" },
-   
- { nome:"MTD 535-24 IR",
+ {
+  nome:"Mayones Jabba 5 IR",
+  preco:89,
+  link:"https://pay.cakto.com.br/388nbge_735148",
+  desc:"Flagship europeu com profundidade e definição profissional.",
+  release:"2026-01-23T19:00:00",
+  audio:"/audio/mayones-jabba-5.mp3"
+},
+
+{
+  nome:"MTD 535-24 IR",
   preco:89,
   link:"https://pay.cakto.com.br/3bgf2sv_735157",
   desc:"Boutique luthier com dinâmica extrema e médios orgânicos.",
-  release:"2026-01-23T19:00:00", audio:"/audio/mtd-535-24"
-},
+  release:"2026-01-23T19:00:00",
+  audio:"/audio/mtd-535-24.mp3"
+}
   { nome:"Warwick Corvette IR", preco:69, link:"https://pay.cakto.com.br/3frq3qm_719724", desc:"Médio encorpado e ataque metálico.", audio:"/audio/warwick-corvette.mp3" },
   { nome:"Ken Smith IR", preco:69, link:"https://pay.cakto.com.br/zy8esjf_719715", desc:"Resposta hi-fi, ultra definição e sustain perfeito.", audio:"/audio/ken-smith.mp3" }
 ];
