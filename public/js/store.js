25e28516d43ecfc560d967ec4ec86b521adde5fc
ELBS <86670001+nigevolution@users.noreply.github.com>
2026-05-03 15:38:34 -0400

    Update video link for TrackPilot in store.js

--- public/js/store.js
+++ public/js/store.js
@@ -26,7 +26,7 @@ const produtos = [
     preco:59,
     link:"https://pay.cakto.com.br/p8ufknn_866145",
     desc:"Importação inteligente para REAPER. Organize, adicione e importe seus áudios nas tracks certas em segundos.",
-    video:"/videos/demonstracao-script-trackpilot.mp4"
+    video:"/videos/trackpilot.mp4"
   },
   {
     nome:"Bass Mods IR",
